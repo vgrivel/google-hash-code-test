1216adda6a598d1b463f7842bbb58e9298e8afe3
Vincent Grivel <vincent.grivel@x-apps.com>
2016-02-11 20:49:44 +0100

    Order warehouse

--- src/main/java/ch/daplab/google/App.java
+++ src/main/java/ch/daplab/google/App.java
@@ -1,13 +1,14 @@
 package ch.daplab.google;
 
-import ch.daplab.google.Algo.AlgoNaive;
+import ch.daplab.google.dorwi.Helper;
 import ch.daplab.google.input.Container;
 import ch.daplab.google.input.Input;
-import ch.daplab.google.utils.Utils;
+import ch.daplab.google.input.Order;
+import ch.daplab.google.input.Warehouse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.util.Map;
 
 /**
  * Created by vincent on 6/25/15.
@@ -16,8 +17,10 @@ public class App {
     private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
     public static void main(String args[]) throws Exception {
-       Input input = new Input();
+        Input input = new Input();
         Container container = input.loadData("busy_day.in");
+
+
 
     }
 }
