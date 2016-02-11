cd47227437897f2690dfe485b61b3d1ee7ce1d60
dorwi <vidor.kanalas@gmail.com>
2016-02-11 19:35:20 +0100

    add commands

---
+++ src/main/java/ch/daplab/google/dorwi/Runner.java
@@ -0,0 +1,10 @@
+package ch.daplab.google.dorwi;
+
+/**
+ * Created by dori on 11.02.16.
+ */
+public class Runner {
+    public static void main(String[] args){
+        System.out.println("Running");
+    }
+}
